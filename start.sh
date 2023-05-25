489919c97d3d185329e5080040194c18a9644aa1
Tao <59834693+worldofgeese@users.noreply.github.com>
2023-05-25 17:16:35 +0200

    Use k3s instead of k3d

--- start.sh
+++ start.sh
@@ -6,19 +6,29 @@ set -e
 command -v rsync &> /dev/null || sudo apt-get install -y rsync
 
 # Install Garden if it's not already installed
-command -v garden &> /dev/null || (curl -sSL https://github.com/garden-io/garden/releases/download/edge-bonsai/garden-bonsai-linux-amd64.tar.gz | tar xz && mv linux-amd64/* /usr/local/bin/garden)
+command -v garden &> /dev/null || (curl -sSL https://github.com/garden-io/garden/releases/download/0.13.0-1/garden-0.13.0-linux-amd64.tar.gz | tar xz && \
+sudo mv linux-amd64/* /usr/local/bin)
 
-# Install k3d if it's not already installed
-command -v k3d &> /dev/null || (curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash)
-
-# Check if a k3d cluster with the name k3s-default already exists
-if k3d cluster list | grep -q k3s-default; then
-  echo "A k3d cluster with the name k3s-default already exists. Skipping cluster creation."
+# Download k3s if it's not already installed
+if [ ! -f "./k3s" ]; then
+    wget -O k3s https://github.com/k3s-io/k3s/releases/download/v1.27.1%2Bk3s1/k3s
+    chmod +x k3s
 else
-  # Create a k3d cluster
-  k3d cluster create --k3s-arg '--disable=traefik@server:0' --network host
+    # Check if k3s is already running and stop it
+    if ps aux | grep '[k]3s server' > /dev/null; then
+        sudo kill $(ps aux | grep '[k]3s server' | awk '{print $2}')
+    fi
 fi
 
+# Start k3s server with host Docker iamge support and Traefik ingress controller disabled
+nohup sudo ./k3s server --docker --disable=traefik --snapshotter native > /dev/null 2>&1 &
+
+# Copy k3s config to user's home directory
+mkdir -p ~/.kube && \
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && \
+sudo chmod 644 ~/.kube/config
+
+# Do not install NGINX ingress controller
 sed -i 's/\(providers:\)/\1\n  - name: local-kubernetes\n    environments: [local]\n    namespace: ${environment.namespace}\n    defaultHostname: ${var.base-hostname}\n    setupIngressController: null/' project.garden.yml
 
 # Update the garden.yml file for the vote container
