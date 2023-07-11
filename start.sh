bb2aa7eacb3dbf31fb8c229133640a079386bd69
Tao <59834693+worldofgeese@users.noreply.github.com>
2023-07-11 17:04:29 +0200

    Use latest `garden` always

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ set -e
 command -v rsync &> /dev/null || sudo apt-get install -y rsync
 
 # Install Garden if it's not already installed
-command -v garden &> /dev/null || (curl -sSL https://github.com/garden-io/garden/releases/download/0.13.0/garden-0.13.0-linux-amd64.tar.gz | tar xz && \
+command -v garden &> /dev/null || (curl -sSL https://github.com/garden-io/garden/releases/latest/download/garden-0.13.7-linux-amd64.tar.gz | tar xz && \
 sudo mv linux-amd64/* /usr/local/bin)
 
 # Download k3s if it's not already installed
